944d234610fb812ed03ab20d7f826733ffef6086
Hiroki Shiota <yuki041137784@gmail.com>
2021-01-30 04:24:19 +0900

    Add sql connection test

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-  fmt.Println("Hello, World!")
+    db, err := sql.Open("mysql", "root@/gosample")
+    if err != nil {
+        panic(err.Error())
+    }
+    defer db.Close()
+
+    rows, err := db.Query("SELECT * FROM users")
+    defer rows.Close()
+    if err != nil {
+        panic(err.Error())
+    }
+
+    for rows.Next() {
+        var id int
+        var name string
+        if err := rows.Scan(&id, &name); err != nil {
+            panic(err.Error())
+        }
+        fmt.Println(id, name)
+    }
 }
